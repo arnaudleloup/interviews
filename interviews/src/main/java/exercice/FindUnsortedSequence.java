1126652bdd9640effaac387045cd7a34433035be
arnaudleloup <leloup.arnaud@gmail.com>
2014-08-09 13:24:19 +0200

    Add FindUnsortedSequence

---
+++ interviews/src/main/java/exercice/FindUnsortedSequence.java
@@ -0,0 +1,85 @@
+package exercice;
+
+/**
+ * Given an array of integers, write a method to find indices m and n
+ * such that if you sorted elements m through n, the entire array would be sorted.
+ * Minimize n - m.
+ *
+ * O(n) / O(1)
+ * ~ 4n / ~ 1
+ */
+public class FindUnsortedSequence {
+
+	public static int[] f(int[] t) {
+		int m = findM(t);
+		int n = findN(t);
+
+		int[] ret = {m, n};
+		return ret;
+	}
+
+	/**
+	 * O(n) / O(1)
+	 * ~ 2n / ~ 1
+	 */
+	private static int findM(int[] t) {
+		int i = 1;
+		int previous = t[0];
+		while (i < t.length && t[i] >= previous) {
+			previous = t[i];
+			i++;
+		}
+
+		if (i == t.length) {
+			return -1;
+		}
+
+		int min = Integer.MAX_VALUE;
+		while (i < t.length) {
+			if (t[i] < min) {
+				min = t[i];
+			}
+			i++;
+		}
+
+		i = 0;
+		while (t[i] <= min) {
+			i++;
+		}
+
+		return i;
+	}
+
+	/**
+	 * O(n) / O(1)
+	 * ~ 2n / ~ 1
+	 */
+	private static int findN(int[] t) {
+		int j = t.length - 2;
+		int previous = t[t.length - 1];
+		while (j >= 0 && t[j] <= previous) {
+			previous = t[j];
+			j--;
+		}
+
+		if (j == -1) {
+			return -1;
+		}
+
+		int max = -Integer.MAX_VALUE;
+		while (j >= 0) {
+			if (t[j] > max) {
+				max = t[j];
+			}
+
+			j--;
+		}
+
+		j = t.length - 1;
+		while (t[j] >= max) {
+			j--;
+		}
+
+		return j;
+	}
+}
---
+++ interviews/src/test/java/exercice/FindUnsortedSequenceTest.java
@@ -0,0 +1,26 @@
+package exercice;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class FindUnsortedSequenceTest {
+
+	@Test
+	public void test() {
+		int[] t = {1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19};
+		int[] expected = {3, 9};
+		int[] actual = FindUnsortedSequence.f(t);
+		assertTrue(Arrays.toString(actual), Arrays.equals(expected, actual));
+	}
+
+	@Test
+	public void test2() {
+		int[] t = {1, 2, 4, 7, 10, 11};
+		int[] expected = {-1, -1};
+		int[] actual = FindUnsortedSequence.f(t);
+		assertTrue(Arrays.toString(actual), Arrays.equals(expected, actual));
+	}
+}
