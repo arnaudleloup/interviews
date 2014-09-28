28c0e76676d00a4c916ae358a178de396b98c20c
arnaudleloup <leloup.arnaud@gmail.com>
2014-09-28 21:11:19 +0200

    Add DuplicatesExceptOne

---
+++ interviews/src/main/java/exercice/DuplicatesExceptOne.java
@@ -0,0 +1,92 @@
+package exercice;
+
+import java.util.Random;
+
+/**
+ * Given an array of integers where every element appears k > 1 times except for one that appears
+ * once, find that unique element.
+ */
+public class DuplicatesExceptOne {
+
+	/**
+	 * Quick select based implementation.
+	 * O(n) / O(1)
+	 */
+	public static int f(int[] t, int k) {
+		shuffle(t);
+
+		int lo = 0;
+		int hi = t.length - 1;
+
+		while (true) {
+			int p = partition(t, lo, hi);
+
+			if (p % k == 0) {
+				// If p = q * k then all elements before p
+				// are k duplicates.
+				// t[p] is unique or a k duplicate too
+				if (isUnique(t, p, hi)) { // O(hi -lo) / O(1)
+					return t[p];
+				} else {
+					// if t[p] is a k duplicate
+					// we partition all its duplicates
+					lo = partitionDuplicates(t, p, hi); // O(hi - lo) / O(1)
+				}
+			} else {
+				hi = p - 1;
+			}
+		}
+	}
+
+	private static boolean isUnique(int[] t, int lo, int hi) {
+		for (int i = lo + 1; i <= hi; i++) {
+			if (t[i] == t[lo]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private static int partition(int[] t, int lo, int hi) {
+		int p = hi;
+		int firstHigh = lo;
+
+		for (int i = lo; i <= hi; i++) {
+			if (t[i] < t[p]) {
+				swap(t, firstHigh, i);
+				firstHigh++;
+			}
+		}
+
+		swap(t, firstHigh, p);
+		return firstHigh;
+	}
+
+	private static int partitionDuplicates(int[] t, int p, int hi) {
+		int firstHigh = p + 1;
+
+		for (int i = p + 1; i <= hi; i++) {
+			if (t[i] == t[p]) {
+				swap(t, firstHigh, i);
+				firstHigh++;
+			}
+		}
+
+		return firstHigh;
+	}
+
+	private static void shuffle(int[] t) {
+		Random random = new Random();
+		for (int i = 0; i < t.length - 1; i++) {
+			int r = random.nextInt(t.length);
+			swap(t, i, r);
+		}
+	}
+
+	private static void swap(int[] t, int i, int j) {
+		int temp = t[i];
+		t[i] = t[j];
+		t[j] = temp;
+	}
+}
---
+++ interviews/src/test/java/exercice/DuplicatesExceptOneTest.java
@@ -0,0 +1,29 @@
+package exercice;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DuplicatesExceptOneTest {
+
+	@Test
+	public void test() {
+		int[] t = {3, 2, 3, 4, 2, 5, 3, 2, 5, 5};
+		int k = 3;
+		int expected = 4;
+		assertEquals(expected, DuplicatesExceptOne.f(t, k));
+	}
+
+	@Test
+	public void test2() {
+		int[] t = {1, 1, 1, 1, 1, 1,
+				2, 2, 2, 2, 2, 2,
+				3,
+				4, 4, 4, 4, 4, 4,
+				5, 5, 5, 5, 5, 5,
+				7, 7, 7, 7, 7, 7};
+		int k = 6;
+		int expected = 3;
+		assertEquals(expected, DuplicatesExceptOne.f(t, k));
+	}
+}
