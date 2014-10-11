f413d18e73facc45a106e6d59e371b63d0ec3268
arnaudleloup <leloup.arnaud@gmail.com>
2014-10-11 13:14:56 +0200

    Add Rotation

---
+++ interviews/src/main/java/array/Rotation.java
@@ -0,0 +1,77 @@
+package array;
+
+/**
+ * Rotate (shift) an array by d elements.
+ */
+public class Rotation {
+
+	public static void rotate(int[] a, int d) {
+		int n = a.length;
+
+		if (d > n || d < n) {
+			d = d % n;
+		}
+
+		if (d < 0) {
+			d = n + d;
+		}
+
+		int blocks = n / d;
+		for (int i = 1; i < blocks; i++) {
+			for (int j = 0; j < d; j++) {
+				swap(a, j, i * d + j);
+			}
+		}
+
+		// Last block is partial
+		int left = n - blocks * d;
+		if (left > 0) {
+			for (int i = 0; i < left; i++) {
+				swap(a, i, n - left + i);
+			}
+
+			rotate(a, d - left, 0, d - 1);
+		}
+	}
+
+	/**
+	 * Rotates only the elements included between lo and hi (both inclusive)
+	 */
+	public static void rotate(int[] a, int d, int lo, int hi) {
+		int n = hi - lo + 1;
+
+		if (d > n || d < n) {
+			d = d % n;
+		}
+
+		if (d < 0) {
+			d = n + d;
+		}
+
+		int blocks = n / d;
+		for (int i = 1; i < blocks; i++) {
+			for (int j = 0; j < d; j++) {
+				swap(a, lo + j, lo + i * d + j);
+			}
+		}
+
+		// Last block is partial
+		int left = n - blocks * d;
+		if (left > 0) {
+			for (int i = 0; i < left; i++) {
+				swap(a, lo + i, lo + n - left + i);
+			}
+
+			rotate(a, d - left, lo, lo + d - 1);
+		}
+	}
+
+	private static void swap(int[] a, int i, int j) {
+		int n = a.length;
+		int k = j % n;
+
+		int temp = a[i];
+		a[i] = a[k];
+		a[k] = temp;
+	}
+}
---
+++ interviews/src/test/java/array/RotationTest.java
@@ -0,0 +1,84 @@
+package array;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class RotationTest {
+
+	@Test
+	public void test() {
+		int[] a = {1, 2, 3, 4, 5, 6, 7};
+		int d = 3;
+		int[] expected = {5, 6, 7, 1, 2, 3, 4};
+
+		Rotation.rotate(a, d);
+		assertTrue(Arrays.equals(expected, a));
+	}
+
+	@Test
+	public void test2() {
+		int[] a = {1, 2, 3, 4, 5, 6, 7};
+		int d = 2;
+		int[] expected = {6, 7, 1, 2, 3, 4, 5};
+
+		Rotation.rotate(a, d);
+		assertTrue(Arrays.equals(expected, a));
+	}
+
+	@Test
+	public void test3() {
+		int[] a = {1, 2, 3, 4, 5, 6, 7};
+		int d = 8;
+		int[] expected = {7, 1, 2, 3, 4, 5, 6};
+
+		Rotation.rotate(a, d);
+		assertTrue(Arrays.equals(expected, a));
+	}
+
+	@Test
+	public void test4() {
+		int[] a = {1, 2, 3, 4, 5, 6, 7};
+		int d = -8;
+		int[] expected = {2, 3, 4, 5, 6, 7, 1};
+
+		Rotation.rotate(a, d);
+		assertTrue(Arrays.equals(expected, a));
+	}
+
+	@Test
+	public void test4Bis() {
+		int[] a = {2, 3, 4, 5, 6};
+		int d = 3;
+		int[] expected = {4, 5, 6, 2, 3};
+
+		Rotation.rotate(a, d);
+		assertTrue(Arrays.equals(expected, a));
+	}
+
+
+	@Test
+	public void test5() {
+		int[] a = {1, 2, 3, 4, 5, 6, 7};
+		int d = 2;
+		int lo = 1;
+		int hi = 5;
+		int[] expected = {1, 5, 6, 2, 3, 4, 7};
+
+		Rotation.rotate(a, d, lo, hi);
+		assertTrue(Arrays.equals(expected, a));
+	}
+	@Test
+	public void test6() {
+		int[] a = {1, 2, 3, 4, 5, 6, 7};
+		int d = 3;
+		int lo = 1;
+		int hi = 5;
+		int[] expected = {1, 4, 5, 6, 2, 3, 7};
+
+		Rotation.rotate(a, d, lo, hi);
+		assertTrue(Arrays.equals(expected, a));
+	}
+}
