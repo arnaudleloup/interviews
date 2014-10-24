00e5a609e62988bfcc4bb47f9fe6fad70c2c2b84
arnaudleloup <leloup.arnaud@gmail.com>
2014-10-24 19:45:30 +0200

    Sample renamed ReservoirSample

--- interviews/src/main/java/misc/ReservoirSample.java
+++ interviews/src/main/java/misc/ReservoirSample.java
@@ -2,14 +2,22 @@ package misc;
 
 import java.util.Random;
 
-public class Sample {
+/**
+ * Provides two methods:
+ * <ul>
+ * <li> void add(in e) to add an element into this reservoir sample </li>
+ * <li> void int[] sample() to retrieve K elements already added.
+ * The probability that an element is returned by this method must be uniform. </li>
+ * </ul>
+ */
+public class ReservoirSample {
 
 	private final int[] bag;
 	private final Random r = new Random();
 	private final int K;
 	private int i = 0;
 
-	public Sample(int K) {
+	public ReservoirSample(int K) {
 		this.K = K;
 		bag = new int[K];
 	}
