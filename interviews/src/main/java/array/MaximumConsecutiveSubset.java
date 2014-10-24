4f39242b0d205c6889b46200768ee1e9a592776c
arnaudleloup <leloup.arnaud@gmail.com>
2014-10-24 20:15:31 +0200

    Update MaximumConsecutiveSubset

--- interviews/src/main/java/array/MaximumConsecutiveSubset.java
+++ interviews/src/main/java/array/MaximumConsecutiveSubset.java
@@ -1,6 +1,11 @@
 package array;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 /**
  * Given a set of numbers, find the longest subset with consecutive numbers.
@@ -51,6 +56,66 @@ public class MaximumConsecutiveSubset {
 			}
 
 			k++;
+		}
+
+		return subset;
+	}
+
+	/**
+	 * Time complexity : O(n)
+	 * Space complexity : O(n)
+	 */
+	public static int[] f2(int[] a) {
+		int n = a.length;
+
+		Set<Integer> saw = new HashSet<>();
+		Map<Integer, Integer> lower = new HashMap<>();
+		Map<Integer, Integer> higher = new HashMap<>();
+
+		for (int i = 0; i < n; i++) {
+			int e = a[i];
+
+			if (!saw.contains(e)) {
+				if (lower.containsKey(e - 1) && higher.containsKey(e + 1)) {
+					int lo = lower.get(e - 1);
+					int hi = higher.get(e + 1);
+					higher.put(lo, hi);
+					lower.put(hi, lo);
+					higher.remove(e + 1);
+					lower.remove(e - 1);
+				} else if (lower.containsKey(e - 1)) {
+					int lo = lower.get(e - 1);
+					lower.put(e, lo);
+					higher.put(lo, e);
+					lower.remove(e - 1);
+				} else if (higher.containsKey(e + 1)) {
+					int hi = higher.get(e + 1);
+					higher.put(e, hi);
+					lower.put(hi, e);
+					higher.remove(e + 1);
+				} else {
+					lower.put(e, e);
+					higher.put(e, e);
+				}
+
+			}
+
+			saw.add(e);
+		}
+
+		int lo = 0;
+		int hi = 0;
+
+		for (Entry<Integer, Integer> entry : higher.entrySet()) {
+			if (entry.getValue() - entry.getKey() > hi - lo) {
+				lo = entry.getKey();
+				hi = entry.getValue();
+			}
+		}
+
+		int[] subset = new int[hi - lo + 1];
+		for (int i = lo; i <= hi; i++) {
+			subset[i - lo] = i;
 		}
 
 		return subset;
--- interviews/src/test/java/array/MaximumConsecutiveSubsetTest.java
+++ interviews/src/test/java/array/MaximumConsecutiveSubsetTest.java
@@ -15,4 +15,20 @@ public class MaximumConsecutiveSubsetTest {
 		int[] actual = MaximumConsecutiveSubset.f(a);
 		assertArrayEquals(Arrays.toString(actual), expected, actual);
 	}
+
+	@Test
+	public void test2() {
+		int[] a = {5, 1, 9, 3, 8, 20, 4, 10, 2, 11, 3};
+		int[] expected = {1, 2, 3, 4, 5};
+		int[] actual = MaximumConsecutiveSubset.f2(a);
+		assertArrayEquals(Arrays.toString(actual), expected, actual);
+	}
+
+	@Test
+	public void test3() {
+		int[] a = {3, 8, 15, 22, 5, 7, 99, 9, 0, 4, 78, 18, 6};
+		int[] expected = {3, 4, 5, 6, 7, 8, 9};
+		int[] actual = MaximumConsecutiveSubset.f2(a);
+		assertArrayEquals(Arrays.toString(actual), expected, actual);
+	}
 }
