c7cda77d0123815d352ec78d302fa5d70cecb858
arnaudleloup <leloup.arnaud@gmail.com>
2014-11-03 19:38:14 +0100

    update AppointmentsConflict

--- interviews/src/main/java/array/AppointmentsConflict.java
+++ interviews/src/main/java/array/AppointmentsConflict.java
@@ -13,36 +13,19 @@ public class AppointmentsConflict {
 	 * Returns the appointments in conflicts.
 	 * Two appointments are in conflicts if the ending time of one is after
 	 * the beginning time of the other.
+	 * The appointments given as input are sorted by beginning time.
+	 * You can assume that the appointments are sorted.
 	 * 
 	 * Time complexity : O(n * log(n))
 	 * Space complexity : O(n)
 	 */
 	public static Set<Appointment> conflicts(List<Appointment> apps) {
-		Collections.sort(apps, new Comparator<Appointment>() { // sort by beginning time
-
-			@Override
-			public int compare(Appointment a1, Appointment a2) {
-				if (a1.beginning < a2.beginning) {
-					return -1;
-				}
-				return 1;
-			}
-		});
-
-		PriorityQueue<Appointment> heap = new PriorityQueue<>(8, new Comparator<Appointment>() {
-
-			@Override
-			public int compare(Appointment a1, Appointment a2) { // min heap on ending time
-				if (a1.ending < a2.ending) {
-					return -1;
-				}
-				return 1;
-			}
-		});
+		Collections.sort(apps, new Appointment.StartComparator()); // Assume that appointments sorted by start
+		PriorityQueue<Appointment> heap = new PriorityQueue<>(8, new Appointment.FinishComparator());
 
 		Set<Appointment> conflicts = new HashSet<>();
 		for (Appointment a : apps) {
-			while (!heap.isEmpty() && heap.peek().ending <= a.beginning) {
+			while (!heap.isEmpty() && heap.peek().finish <= a.start) {
 				heap.poll();
 			}
 
@@ -60,12 +43,78 @@ public class AppointmentsConflict {
 	}
 
 	public static class Appointment {
-		int beginning;
-		int ending;
+		int start;
+		int finish;
 
-		public Appointment(int beginning, int ending) {
-			this.beginning = beginning;
-			this.ending = ending;
+		public Appointment(int start, int finish) {
+			this.start = start;
+			this.finish = finish;
+		}
+
+		public static class StartComparator implements Comparator<Appointment> {
+			@Override
+			public int compare(Appointment a1, Appointment a2) {
+				if (a1.start < a2.start) {
+					return -1;
+				}
+				return 1;
+			}
+		}
+
+		public static class InverseStartComparator implements Comparator<Appointment> {
+			@Override
+			public int compare(Appointment a1, Appointment a2) {
+				if (a1.start < a2.start) {
+					return 1;
+				}
+				return -1;
+			}
+		}
+
+		public static class FinishComparator implements Comparator<Appointment> {
+			@Override
+			public int compare(Appointment a1, Appointment a2) {
+				if (a1.finish < a2.finish) {
+					return -1;
+				}
+				return 1;
+			}
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + start;
+			result = prime * result + finish;
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null) {
+				return false;
+			}
+			if (getClass() != obj.getClass()) {
+				return false;
+			}
+			Appointment other = (Appointment) obj;
+			if (start != other.start) {
+				return false;
+			}
+			if (finish != other.finish) {
+				return false;
+			}
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "Appointment [beginning=" + start + ", ending=" + finish
+					+ "]";
 		}
 	}
 }
--- interviews/src/test/java/array/AppointmentsConflictTest.java
+++ interviews/src/test/java/array/AppointmentsConflictTest.java
@@ -14,22 +14,20 @@ public class AppointmentsConflictTest {
 
 	@Test
 	public void test() {
-		List<Appointment> apps = Arrays.asList(
-				new Appointment(2, 6),
-				new Appointment(3, 8),
-				new Appointment(7, 9)
-				);
+		Appointment a1 = new Appointment(2, 6);
+		Appointment a2 = new Appointment(3, 8);
+		Appointment a3 = new Appointment(7, 9);
+		List<Appointment> apps = Arrays.asList(a1, a2, a3);
 
 		assertEquals(new HashSet<Appointment>(apps), AppointmentsConflict.conflicts(apps));
 	}
 
 	@Test
 	public void test2() {
-		List<Appointment> apps = Arrays.asList(
-				new Appointment(2, 6),
-				new Appointment(10, 15),
-				new Appointment(7, 9)
-				);
+		Appointment a1 = new Appointment(2, 6);
+		Appointment a2 = new Appointment(7, 9);
+		Appointment a3 = new Appointment(10, 15);
+		List<Appointment> apps = Arrays.asList(a1, a2, a3);
 
 		assertEquals(new HashSet<Appointment>(), AppointmentsConflict.conflicts(apps));
 	}
@@ -50,7 +48,7 @@ public class AppointmentsConflictTest {
 		Appointment a2 = new Appointment(3, 7);
 		Appointment a3 = new Appointment(4, 8);
 		Appointment a4 = new Appointment(5, 9);
-		List<Appointment> apps = Arrays.asList(a2, a4, a3, a1);
+		List<Appointment> apps = Arrays.asList(a1, a2, a3, a4);
 
 		assertEquals(new HashSet<Appointment>(apps), AppointmentsConflict.conflicts(apps));
 	}
@@ -62,7 +60,7 @@ public class AppointmentsConflictTest {
 		Appointment a3 = new Appointment(9, 15);
 		Appointment a4 = new Appointment(17, 20);
 		Appointment a5 = new Appointment(1, 30);
-		List<Appointment> apps = Arrays.asList(a2, a4, a3, a5, a1);
+		List<Appointment> apps = Arrays.asList(a5, a1, a2, a3, a4);
 
 		assertEquals(new HashSet<Appointment>(apps), AppointmentsConflict.conflicts(apps));
 	}
@@ -73,7 +71,7 @@ public class AppointmentsConflictTest {
 		Appointment a2 = new Appointment(7, 10);
 		Appointment a3 = new Appointment(9, 15);
 		Appointment a4 = new Appointment(17, 20);
-		List<Appointment> apps = Arrays.asList(a2, a4, a3, a1);
+		List<Appointment> apps = Arrays.asList(a1, a2, a3, a4);
 
 		assertEquals(new HashSet<Appointment>(Arrays.asList(a2, a3)), AppointmentsConflict.conflicts(apps));
 	}
