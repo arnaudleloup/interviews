ae70312f8ee28e4b3f8d5fe773172028517ac945
arnaudleloup <leloup.arnaud@gmail.com>
2014-11-11 15:19:20 +0100

    Update Power

--- interviews/src/main/java/number/Power.java
+++ interviews/src/main/java/number/Power.java
@@ -2,11 +2,38 @@ package number;
 
 public class Power {
 
-	public static int power(int a, int b) {
+	/**
+	 * Time complexity: O(b)
+	 * Space complexity: O(1)
+	 */
+	public static int f(int a, int b) {
 		int count = a;
 
 		for (int i = 1; i < b; i++) {
 			count = Multiplication.times(count, a);
+		}
+
+		return count;
+	}
+
+	/**
+	 * Time complexity: O(log(b))
+	 * Space complexity: O(1)
+	 */
+	public static int f2(int a, int b) {
+		int count = 1;
+
+		for (int i = 0; i < 32; i++) {
+			if ((b & 1) == 1) {
+				int temp = a;
+				for (int j = 0; j < i; j++) {
+					temp *= temp;
+				}
+
+				count *= temp;
+			}
+
+			b = b >> 1;
 		}
 
 		return count;
--- interviews/src/test/java/number/PowerTest.java
+++ interviews/src/test/java/number/PowerTest.java
@@ -2,23 +2,36 @@ package number;
 
 import static org.junit.Assert.assertEquals;
 
-import number.Power;
-
 import org.junit.Test;
 
 public class PowerTest {
 
 	@Test
 	public void test() {
-		assertEquals(2, Power.power(2, 1));
-		assertEquals(4, Power.power(2, 2));
-		assertEquals(8, Power.power(2, 3));
-		assertEquals(9, Power.power(3, 2));
-		assertEquals(27, Power.power(3, 3));
-		assertEquals(1000000, Power.power(10, 6));
+		assertEquals(2, Power.f(2, 1));
+		assertEquals(4, Power.f(2, 2));
+		assertEquals(8, Power.f(2, 3));
+		assertEquals(9, Power.f(3, 2));
+		assertEquals(27, Power.f(3, 3));
+		assertEquals(1000000, Power.f(10, 6));
 
 		// a negative
-		assertEquals(-125, Power.power(-5, 3));
-		assertEquals(1000000, Power.power(-10, 6));
+		assertEquals(-125, Power.f(-5, 3));
+		assertEquals(1000000, Power.f(-10, 6));
 	}
+
+	@Test
+	public void test2() {
+		assertEquals(2, Power.f2(2, 1));
+		assertEquals(4, Power.f2(2, 2));
+		assertEquals(8, Power.f2(2, 3));
+		assertEquals(9, Power.f2(3, 2));
+		assertEquals(27, Power.f2(3, 3));
+		assertEquals(1000000, Power.f2(10, 6));
+
+		// a negative
+		assertEquals(-125, Power.f2(-5, 3));
+		assertEquals(1000000, Power.f2(-10, 6));
+	}
+
 }
