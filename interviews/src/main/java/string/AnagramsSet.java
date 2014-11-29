8a506f29762f468d1675ce658a4e9e227a255607
arnaudleloup <leloup.arnaud@gmail.com>
2014-11-29 14:23:11 +0100

    Update AnagramsSet

--- interviews/src/main/java/string/AnagramsSet.java
+++ interviews/src/main/java/string/AnagramsSet.java
@@ -49,6 +49,81 @@ public class AnagramsSet {
 		return anagrams;
 	}
 
+	private static Map<Character, Integer> MAP = new HashMap<>();
+
+	static {
+		MAP.put('c', 2);
+		MAP.put('a', 3);
+		MAP.put('t', 5);
+		MAP.put('d', 7);
+		MAP.put('o', 11);
+		MAP.put('g', 13);
+		MAP.put('e', 17);
+		MAP.put('z', 19);
+	}
+
+	/**
+	 * n = set.size
+	 * L = string average length
+	 * 
+	 * Time complexity: O(n * L)
+	 * Space complexity: O(n)
+	 */
+	public static Set<Set<String>> f2(Set<String> set) {
+		Map<Integer, Set<String>> anagrams = new HashMap<>();
+
+		for(String s : set) {
+			int hash = 1;
+			for (int i = 0; i < s.length(); i++) {
+				char c = s.charAt(i);
+				hash *= MAP.get(c);
+			}
+
+			if (!anagrams.containsKey(hash)) {
+				anagrams.put(hash, new HashSet<String>());
+			}
+
+			anagrams.get(hash).add(s);
+		}
+
+		Set<Set<String>> toRet = new HashSet<>();
+		for (Set<String> s : anagrams.values()) {
+			toRet.add(s);
+		}
+
+		return toRet;
+	}
+
+	/**
+	 * n = set.size
+	 * L = string average length
+	 * 
+	 * Time complexity: O(n * L)
+	 * Space complexity: O(n)
+	 */
+	public static Set<Set<String>> f3(Set<String> set) {
+		Map<String, Set<String>> anagrams = new HashMap<>();
+
+		for(String s : set) {
+			char[] chars = s.toCharArray();
+			Arrays.sort(chars);
+			String sorted = new String(chars);
+
+			if (!anagrams.containsKey(sorted)) {
+				anagrams.put(sorted, new HashSet<String>());
+			}
+
+			anagrams.get(sorted).add(s);
+		}
+
+		Set<Set<String>> toRet = new HashSet<>();
+		for (Set<String> s : anagrams.values()) {
+			toRet.add(s);
+		}
+
+		return toRet;
+	}
+
 	private static class CharFrequency {
 
 		int[] a;
--- interviews/src/test/java/string/AnagramsSetTest.java
+++ interviews/src/test/java/string/AnagramsSetTest.java
@@ -38,4 +38,63 @@ public class AnagramsSetTest {
 
 		assertEquals(expected, AnagramsSet.f(set, 128));
 	}
+
+	@Test
+	public void test2() {
+		Set<String> set = new HashSet<>(Arrays.asList("cat", "dog", "tacc", "ez", "god", "tac", "ogd", "catc"));
+
+		Set<Set<String>> expected = new HashSet<>();
+		Set<String> anagrams1 = new HashSet<>();
+		anagrams1.add("cat");
+		anagrams1.add("tac");
+
+		Set<String> anagrams2 = new HashSet<>();
+		anagrams2.add("dog");
+		anagrams2.add("god");
+		anagrams2.add("ogd");
+
+		Set<String> anagrams3 = new HashSet<>();
+		anagrams3.add("ez");
+
+		Set<String> anagrams4 = new HashSet<>();
+		anagrams4.add("tacc");
+		anagrams4.add("catc");
+
+		expected.add(anagrams1);
+		expected.add(anagrams2);
+		expected.add(anagrams3);
+		expected.add(anagrams4);
+
+		assertEquals(expected, AnagramsSet.f2(set));
+	}
+
+	@Test
+	public void test3() {
+		Set<String> set = new HashSet<>(Arrays.asList("cat", "dog", "tacc", "ez", "god", "tac", "ogd", "catc"));
+
+		Set<Set<String>> expected = new HashSet<>();
+		Set<String> anagrams1 = new HashSet<>();
+		anagrams1.add("cat");
+		anagrams1.add("tac");
+
+		Set<String> anagrams2 = new HashSet<>();
+		anagrams2.add("dog");
+		anagrams2.add("god");
+		anagrams2.add("ogd");
+
+		Set<String> anagrams3 = new HashSet<>();
+		anagrams3.add("ez");
+
+		Set<String> anagrams4 = new HashSet<>();
+		anagrams4.add("tacc");
+		anagrams4.add("catc");
+
+		expected.add(anagrams1);
+		expected.add(anagrams2);
+		expected.add(anagrams3);
+		expected.add(anagrams4);
+
+		assertEquals(expected, AnagramsSet.f3(set));
+	}
+
 }
