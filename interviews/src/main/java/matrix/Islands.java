9a833daf6bb18043f098a8cdd92edf048b2e5fcf
arnaudleloup <leloup.arnaud@gmail.com>
2014-11-30 11:57:51 +0100

    Update Islands

--- interviews/src/main/java/matrix/Islands.java
+++ interviews/src/main/java/matrix/Islands.java
@@ -1,7 +1,16 @@
 package matrix;
 
+/**
+ * Given a matrix A of 0s (water) and 1s (land), return the total number of islands. Two pieces of
+ * land are connected if they touch each other (left/up/down/right, not in diagonal). An island
+ * corresponds to a set of connected lands.
+ */
 public class Islands {
 
+	/**
+	 * Time complexity: O(n * m)
+	 * Space complexity: O(1)
+	 */
 	public static int count(int[][] A) {
 		int count = 0;
 
@@ -11,8 +20,8 @@ public class Islands {
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < m; j++) {
 				if (A[i][j] == 1) {
-					count++;
 					dfs(A, i, j, n, m);
+					count++;
 				}
 			}
 		}
@@ -21,23 +30,22 @@ public class Islands {
 	}
 
 	private static void dfs(int[][] A, int i, int j, int n, int m) {
-		if (A[i][j] == 1) {
-			A[i][j] = -1;
-			if (i > 0) {
-				dfs(A, i - 1, j, n, m);
-			}
+		A[i][j] = -1;
 
-			if (i < n - 1) {
-				dfs(A, i + 1, j, n, m);
-			}
+		if (i > 0 && A[i - 1][j] == 1) {
+			dfs(A, i - 1, j, n, m);
+		}
 
-			if (j > 0) {
-				dfs(A, i, j - 1, n, m);
-			}
+		if (i < n - 1 && A[i + 1][j] == 1) {
+			dfs(A, i + 1, j, n, m);
+		}
 
-			if (j < m - 1) {
-				dfs(A, i, j + 1, n, m);
-			}
+		if (j > 0 && A[i][j - 1] == 1) {
+			dfs(A, i, j - 1, n, m);
+		}
+
+		if (j < m - 1 && A[i][j + 1] == 1) {
+			dfs(A, i, j + 1, n, m);
 		}
 	}
 }
