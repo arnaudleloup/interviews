35e40b2097b3dd61c1f293856bf3f12742408400
arnaudleloup <leloup.arnaud@gmail.com>
2014-12-04 19:51:58 +0100

    Update FindPair

--- interviews/src/main/java/array/FindPair.java
+++ interviews/src/main/java/array/FindPair.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import lib.Pair;
+
 /**
  * Write a method to find all pairs of integers within an array
  * which sum to a specified value.
@@ -11,20 +13,22 @@ import java.util.Set;
 public class FindPair {
 
 	/**
-	 * O(n log(n)) / O(1)
+	 * Time complexity: O(n * log(n))
+	 * Space complexity: O(1)
 	 */
-	public static Set<Pair> f(int[] a, int x) {
+	public static Set<Pair<Integer, Integer>> f(int[] a, int x) {
 		Arrays.sort(a);
-		Set<Pair> pairs = new HashSet<>();
 
+		Set<Pair<Integer, Integer>> pairs = new HashSet<>();
+
+		int n = a.length;
 		int lo = 0;
-		int hi = a.length - 1;
-		while (lo < hi) {
-			int c = a[lo] + a[hi];
-			if (c < x) {
+		int hi = n - 1;
+		while (lo <= hi) {
+			if (a[lo] + a[hi] < x) {
 				lo++;
-			} else if (c == x) {
-				pairs.add(new Pair(a[lo++], a[hi--]));
+			} else if (a[lo] + a[hi] == x) {
+				pairs.add(new Pair<Integer, Integer>(a[lo++], a[hi--]));
 			} else {
 				hi--;
 			}
@@ -34,81 +38,23 @@ public class FindPair {
 	}
 
 	/**
-	 * O(n) / O(n)
+	 * Time complexity: O(n)
+	 * Space complexity: O(n)
 	 */
-	public static Set<Pair> f2(int[] a, int x) {
-		Set<Pair> pairs = new HashSet<>();
-		Set<Integer> elements = new HashSet<>();
+	public static Set<Pair<Integer, Integer>> f2(int[] a, int x) {
+		Set<Pair<Integer, Integer>> pairs = new HashSet<>();
 
-		boolean foundHalf = false;
-		for (int i = 0; i < a.length; i++) {
-			if (2 * a[i] == x) { // we have to treat here the case of the half
-				if (foundHalf) {
-					pairs.add(new Pair(a[i], a[i]));
-				} else {
-					foundHalf = true;
-				}
-			} else {
-				elements.add(a[i]);
+		Set<Integer> saw = new HashSet<>();
+		int n = a.length;
+		for (int i = 0; i < n; i++) {
+			int v = a[i];
+			if (saw.contains(x - v)) {
+				pairs.add(new Pair<Integer, Integer>(v, x - v));
 			}
-		}
 
-		for (int i : elements) {
-			if (elements.contains(x - i)) {
-				if (i < x - i) {
-					pairs.add(new Pair(i, x - i));
-				} else {
-					pairs.add(new Pair(x - i, i));
-				}
-			}
+			saw.add(v);
 		}
 
 		return pairs;
-	}
-
-	public static class Pair {
-		int x;
-		int y;
-
-		public Pair(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + x;
-			result = prime * result + y;
-			return result;
-		}
-
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) {
-				return true;
-			}
-			if (obj == null) {
-				return false;
-			}
-			if (getClass() != obj.getClass()) {
-				return false;
-			}
-			Pair other = (Pair) obj;
-			if (x != other.x) {
-				return false;
-			}
-			if (y != other.y) {
-				return false;
-			}
-			return true;
-		}
-
-		@Override
-		public String toString() {
-			return "Pair [x=" + x + ", y=" + y + "]";
-		}
 	}
 }
--- interviews/src/test/java/array/FindPairTest.java
+++ interviews/src/test/java/array/FindPairTest.java
@@ -4,9 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Set;
 
+import lib.Pair;
+
 import org.junit.Test;
 
-import array.FindPair.Pair;
 
 public class FindPairTest {
 
@@ -14,11 +15,11 @@ public class FindPairTest {
 	public void test() {
 		int[] a = {2, 8, 4, 9, 12, -1, -4, 10, -9, 3, 2, 4, 0};
 		int x = 8;
-		Set<Pair> pairs1 = FindPair.f(a, x);
+		Set<Pair<Integer, Integer>> pairs1 = FindPair.f(a, x);
 
 		assertEquals(4, pairs1.size());
 
-		Set<Pair> pairs2 = FindPair.f2(a, x);
+		Set<Pair<Integer, Integer>> pairs2 = FindPair.f2(a, x);
 		assertEquals(4, pairs2.size());
 	}
 }
