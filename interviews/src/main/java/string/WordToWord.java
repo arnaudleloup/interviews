d57da6f5ce3eae1520e4caf56be05e8a4b867ba1
arnaudleloup <leloup.arnaud@gmail.com>
2014-12-07 15:56:23 +0100

    Update WordToWord

--- interviews/src/main/java/string/WordToWord.java
+++ interviews/src/main/java/string/WordToWord.java
@@ -2,7 +2,6 @@ package string;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -25,75 +24,66 @@ public class WordToWord {
 
 	/**
 	 * n = number of words into the dictionary
-	 * L = word length
+	 * L = average word length
 	 * Time complexity: O(n * L)
 	 * Space complexity: O(n)
 	 */
-	public static List<String> f(Set<String> dictionnary, String start, String end) {
-		if (start.equals(end)) {
-			return Arrays.asList(start);
-		}
-
+	public static List<String> f(Set<String> dictionary, String start, String end) {
 		Map<String, String> parent = new HashMap<>();
-		Set<String> marked = new HashSet<>();
+		parent.put(start, null);
 		Set<String> frontier = new HashSet<>();
 		frontier.add(start);
 
 		while (!frontier.isEmpty()) {
 			Set<String> next = new HashSet<>();
-			for (String s : frontier) {
-				marked.add(s);
 
-				for (String adj : adj(dictionnary, s)) {
-					if (!marked.contains(adj)) {
-						next.add(adj);
-						parent.put(adj, s);
-						if (adj.equals(end)) {
-							break;
-						}
+			for (String s : frontier) {
+				for (String t : adj(dictionary, s)) {
+					if (!parent.containsKey(t)) {
+						parent.put(t, s);
+						next.add(t);
+					}
+
+					if (t.equals(end)) {
+						break;
 					}
 				}
 			}
 
 			frontier = next;
-			if (frontier.contains(end)) {
-				break;
-			}
 		}
 
-		if (!frontier.contains(end)) {
-			return new ArrayList<>();
+		if (!parent.containsKey(end)) {
+			return new ArrayList<String>();
 		}
 
 		Deque<String> stack = new ArrayDeque<>();
-		String s = end;
-		while (s != null) {
-			stack.addLast(s);
-			s = parent.get(s);
+		String current = end;
+		while (current != null) {
+			stack.addLast(current);
+			current = parent.get(current);
 		}
 
-		List<String> list = new ArrayList<>();
+		List<String> ret = new ArrayList<>();
 		while (!stack.isEmpty()) {
-			list.add(stack.removeLast());
+			ret.add(stack.pollLast());
 		}
 
-		return list;
+		return ret;
 	}
 
 	/**
 	 * Time complexity : O(L)
 	 * Space complexity : O(1)
 	 */
-	private static Set<String> adj(Set<String> dictionnary, String word) {
-		Set<String> adjs = new HashSet<>();
+	private static Set<String> adj(Set<String> dictionary, String s) {
+		Set<String>  adjs = new HashSet<>();
 
-		for (int i = 0; i < word.length(); i++) {
-			for (char c = 'A'; c <= 'Z'; c++) {
-				if (c != word.charAt(i)) {
-					String adj = word.substring(0, i) + c + word.substring(i + 1, word.length());
-					if (dictionnary.contains(adj)) {
-						adjs.add(adj);
-					}
+		for (int i = 0; i < s.length(); i++) {
+			for (char c = 0; c < 256; c++) { // ASCII
+				String t = s.substring(0, i) + c + s.substring(i + 1, s.length());
+				if (dictionary.contains(t)) {
+					adjs.add(t);
 				}
 			}
 		}
