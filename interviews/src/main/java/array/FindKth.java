d467908c1f5ed31baa0e17b3f8baa6c2af5d0a8e
arnaudleloup <leloup.arnaud@gmail.com>
2014-12-15 17:01:41 +0100

    Update FindKth

--- interviews/src/main/java/array/FindKth.java
+++ interviews/src/main/java/array/FindKth.java
@@ -9,30 +9,31 @@ public class FindKth {
 
 	/**
 	 * Quick select based implementation.
-	 * O(n) / O(1)
+	 * 
+	 * Time complexity: O(n)
+	 * Space complexity: O(1)
 	 */
 	public static int f(int[] a, int k) {
 		shuffle(a);
-		select(a, 0, a.length - 1, k);
+
+		int lo = 0;
+		int hi = a.length - 1;
+		int m = -1;
+		while (m != k) {
+			m = partition(a, lo, hi);
+			if (m < k) {
+				lo = m + 1;
+			} else if (m > k) {
+				hi = m - 1;
+			}
+		}
+
 		return a[k];
 	}
 
-	private static void select(int[] a, int lo, int hi, int k) {
-		int i = partition(a, lo, hi);
-
-		if (i < k) {
-			select(a, i + 1, hi, k);
-		} else if (i == k) {
-			return;
-		} else {
-			select(a, lo, i - 1, k);
-		}
-	}
-
 	private static int partition(int[] a, int lo, int hi) {
-		int p = hi;
+		int p = hi; // pivot
 		int firstHigh = lo;
-
 		for (int i = lo; i <= hi; i++) {
 			if (a[i] < a[p]) {
 				swap(a, i, firstHigh++);
@@ -51,7 +52,7 @@ public class FindKth {
 
 	private static void shuffle(int[] a) {
 		Random r = new Random();
-		for (int i = a.length - 1; i > 0; i--) {
+		for (int i = a.length - 1; i >= 0; i--) {
 			int j = r.nextInt(i + 1);
 			swap(a, i, j);
 		}
