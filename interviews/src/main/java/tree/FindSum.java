6e1c57adafff02bd2acd7c0e35c36befd6cd05cd
arnaudleloup <leloup.arnaud@gmail.com>
2014-12-15 19:26:31 +0100

    Update FindSum

--- interviews/src/main/java/tree/FindSum.java
+++ interviews/src/main/java/tree/FindSum.java
@@ -1,6 +1,10 @@
 package tree;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
 import java.util.NavigableSet;
+import java.util.Set;
 
 import lib.Pair;
 
@@ -11,23 +15,130 @@ import lib.Pair;
 public class FindSum {
 
 	/**
-	 * O(n log(n)) / O(1)
+	 * Time complexity: O(n * log(n))
+	 * Space complexity: O(1)
 	 */
 	public static Pair<Integer, Integer> f(NavigableSet<Integer> t, int x) {
+		Pair<Integer, Integer> pair = null;
+
 		int lo = t.first();
 		int hi = t.last();
 
 		while (lo <= hi) {
-			if (lo + hi < x) {
-				lo = t.higher(lo);
-			} else if (lo + hi == x) {
-				return new Pair<Integer, Integer>(lo, hi);
+			if (lo + hi == x) {
+				pair = new Pair<>(lo, hi);
+				break;
+			} else if (lo + hi < x) {
+				Integer higher = t.higher(lo);
+				if (higher == null) {
+					break;
+				}
+				lo = higher;
 			} else {
-				hi = t.lower(hi);
+				Integer lower = t.lower(hi);
+				if (lower == null) {
+					break;
+				}
+				hi = lower;
+			}
+		}
+
+		return pair;
+	}
+
+	private Node root;
+
+	public FindSum(int[] a) {
+		for (int e : a) {
+			add(e);
+		}
+	}
+
+	/**
+	 * Time complexity: O(n)
+	 * Space complexity: O(n)
+	 */
+	public Pair<Integer, Integer> find(int x) {
+		Deque<Node> loStack = new ArrayDeque<>();
+		Node lo = min(root, loStack);
+		Deque<Node> hiStack = new ArrayDeque<>();
+		Node hi = max(root, hiStack);
+
+		Set<Node> rightSeen = new HashSet<>();
+		Set<Node> leftSeen = new HashSet<>();
+
+		while (lo.val <= hi.val) {
+			if (lo.val + hi.val == x) {
+				return new Pair<Integer, Integer>(lo.val, hi.val);
+			}
+
+			if (lo.val + hi.val < x) {
+				if (lo.right == null || rightSeen.contains(lo)) {
+					lo = loStack.removeLast();
+				} else {
+					rightSeen.add(lo);
+					lo = min(lo.right, loStack);
+				}
+			} else {
+				if (hi.left == null || leftSeen.contains(hi)) {
+					hi = hiStack.removeLast();
+				} else {
+					leftSeen.add(hi);
+					hi = max(hi.left, hiStack);
+				}
 			}
 		}
 
 		return null;
 	}
 
+	private Node min(Node x, Deque<Node> stack) {
+		Node t = x;
+		while (t.left != null) {
+			stack.addLast(t);
+			t = t.left;
+		}
+		stack.addLast(t);
+
+		return t;
+	}
+
+	private Node max(Node x, Deque<Node> stack) {
+		Node t = x;
+		while (t.right != null) {
+			stack.addLast(t);
+			t = t.right;
+		}
+		stack.addLast(t);
+
+		return t;
+	}
+
+	private void add(int v) {
+		root = add(root, v);
+	}
+
+	private Node add(Node x, int v) {
+		if (x == null) {
+			return new Node(v);
+		}
+
+		if (x.val < v) {
+			x.right = add(x.right, v);
+		} else if (x.val > v) {
+			x.left = add(x.left, v);
+		}
+
+		return x;
+	}
+
+	public static class Node {
+		int val;
+		Node left;
+		Node right;
+
+		public Node(int val) {
+			this.val = val;
+		}
+	}
 }
--- interviews/src/test/java/tree/FindSumTest.java
+++ interviews/src/test/java/tree/FindSumTest.java
@@ -45,4 +45,84 @@ public class FindSumTest {
 		assertNotNull(p);
 		assertEquals(x, p.getX() + p.getY());
 	}
+
+	@Test
+	public void test3() {
+		NavigableSet<Integer> t = new TreeSet<>();
+		t.add(10);
+		t.add(5);
+		t.add(3);
+		t.add(12);
+		t.add(11);
+		t.add(1);
+
+		int x = 100;
+
+		Pair<Integer, Integer> p = FindSum.f(t, x);
+		assertNull(p);
+	}
+
+	@Test
+	public void test4() {
+		NavigableSet<Integer> t = new TreeSet<>();
+		t.add(10);
+		t.add(5);
+		t.add(3);
+		t.add(12);
+		t.add(11);
+		t.add(1);
+
+		int x = -1;
+
+		Pair<Integer, Integer> p = FindSum.f(t, x);
+		assertNull(p);
+	}
+
+	@Test
+	public void test11() {
+		int[] a = {10, 5, 3, 12, 11, 1};
+		FindSum bst = new FindSum(a);
+		int x = 9;
+		Pair<Integer, Integer> p = bst.find(x);
+		assertNull(p);
+	}
+
+	@Test
+	public void test12() {
+		int[] a = {10, 5, 3, 12, 11, 1};
+		FindSum bst = new FindSum(a);
+		int x = 8;
+		Pair<Integer, Integer> p = bst.find(x);
+		assertNotNull(p);
+		assertEquals(x, p.getX() + p.getY());
+	}
+
+	@Test
+	public void test13() {
+		int[] a = {30, 15, 7, 4, 1, 3, 10, 9, 13, 12, 14, 50, 70, 40, 45, 47, 49, 46};
+		FindSum bst = new FindSum(a);
+		int x = 49;
+		Pair<Integer, Integer> p = bst.find(x);
+		assertNotNull(p);
+		assertEquals(x, p.getX() + p.getY());
+	}
+
+	@Test
+	public void test14() {
+		int[] a = {30, 15, 7, 4, 1, 3, 10, 9, 13, 12, 14, 50, 70, 40, 45, 47, 49, 46};
+		FindSum bst = new FindSum(a);
+		int x = 28;
+		Pair<Integer, Integer> p = bst.find(x);
+		assertNotNull(p);
+		assertEquals(x, p.getX() + p.getY());
+	}
+
+	@Test
+	public void test15() {
+		int[] a = {30, 15, 7, 4, 1, 3, 10, 9, 13, 12, 14, 50, 70, 40, 45, 47, 49, 46};
+		FindSum bst = new FindSum(a);
+		int x = 32;
+		Pair<Integer, Integer> p = bst.find(x);
+		assertNull(p);
+	}
 }
