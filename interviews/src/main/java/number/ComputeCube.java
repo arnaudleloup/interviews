e3ef98cfe24129b3a034770942248d084075c2d4
arnaudleloup <leloup.arnaud@gmail.com>
2014-12-15 16:19:59 +0100

    Update ComputeCube

--- interviews/src/main/java/number/ComputeCube.java
+++ interviews/src/main/java/number/ComputeCube.java
@@ -1,9 +1,8 @@
 package number;
 
 import java.util.Arrays;
-
-import datastructure.heap.MinHeap;
-
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Write a program that prints out all integers of the form a^3 + b^3 where a and b are integers
@@ -18,62 +17,78 @@ import datastructure.heap.MinHeap;
 public class ComputeCube {
 
 	/**
-	 * O(n²) / O(n²)
+	 * Time complexity: O(n^2)
+	 * Space complexity: O(n^2)
 	 */
-	public static void f(int n) {
-		int[] t = new int[(n + 1) * (n + 2) / 2];
+	public static int[] f(int n) {
+		int[] a = new int[(n + 1) * (n + 2) / 2];
 		int index = 0;
-		for (int i = 0; i <= n; i++) {
-			int a = i * i * i;
-			for (int j = 0; j <=i; j++) {
-				t[index++] = a + j * j * j;
+		for(int i = 0; i <= n; i++) {
+			int p = i * i * i;
+			for (int j = 0; j <= i; j++) {
+				int q = j * j * j;
+				a[index++] = p + q;
 			}
 		}
 
-		Arrays.sort(t);
-		for (int i = 0; i < t.length; i++) {
-			System.out.print(t[i] + " ");
+		Arrays.sort(a);
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
 		}
 
-		System.out.println();
+		return a;
 	}
 
 	/**
-	 * O(n²log(n)) / O(n)
+	 * Time complexity: O(n^2 * log(n))
+	 * Space complexity: O(n)
 	 */
-	public static void f2(int n) {
-		MinHeap<Tuple> heap = new MinHeap<>();
-
+	public static int[] f2(int n) {
+		PriorityQueue<Element> pq = new PriorityQueue<>(n, new Element.Comp());
 		for (int i = 0; i <= n; i++) {
-			heap.insert(new Tuple(i, 0));
+			pq.add(new Element(i * i * i, i, 0));
 		}
 
-		while (!heap.isEmpty()) { // O(n²) numbers
-			Tuple t = heap.delMin();
-			if (t.j < t.i) {
-				heap.insert(new Tuple(t.i, t.j + 1)); // O(log(n)) to insert
+		int[] a = new int[(n + 1) * (n + 2) / 2];
+		int index = 0;
+		while (!pq.isEmpty()) {
+			Element e = pq.poll();
+			System.out.println(e.value);
+			a[index++] = e.value;
+			int i = e.i;
+			int j = e.j;
+			if (j < i) {
+				int newValue = i * i * i + (j + 1) * (j + 1) * (j + 1);
+				pq.add(new Element(newValue, i, j + 1));
 			}
-
-			System.out.print(t.v + " ");
 		}
 
-		System.out.println();
+		return a;
 	}
 
-	private static class Tuple implements Comparable<Tuple> {
-		int v;
+	private static class Element {
+		int value;
 		int i;
 		int j;
 
-		public Tuple(int i, int j) {
-			this.v = i * i * i + j * j * j;
+		public Element(int value, int i, int j) {
+			this.value = value;
 			this.i = i;
 			this.j = j;
 		}
 
-		@Override
-		public int compareTo(Tuple o) {
-			return Integer.valueOf(v).compareTo(Integer.valueOf(o.v));
+		private static class Comp implements Comparator<Element> {
+
+			@Override
+			public int compare(Element e1, Element e2) {
+				if (e1.value < e2.value) {
+					return -1;
+				} else if (e1.value == e2.value) {
+					return 0;
+				} else {
+					return 1;
+				}
+			}
 		}
 	}
 }
--- interviews/src/test/java/number/ComputeCubeTest.java
+++ interviews/src/test/java/number/ComputeCubeTest.java
@@ -1,5 +1,9 @@
 package number;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class ComputeCubeTest {
@@ -7,7 +11,8 @@ public class ComputeCubeTest {
 	@Test
 	public void test() {
 		int N = 20;
-		ComputeCube.f(N);
-		ComputeCube.f2(N);
+		int[] a = ComputeCube.f(N);
+		int[] b = ComputeCube.f2(N);
+		assertTrue(Arrays.equals(a, b));
 	}
 }
